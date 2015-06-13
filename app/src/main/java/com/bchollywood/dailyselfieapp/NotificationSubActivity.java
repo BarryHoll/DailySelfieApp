d4265097e04b0638dc2231ba4ab44e21a94e52da
BarryHoll <bchollgithub@gmail.com>
2015-06-13 17:34:35 +0100

    Adding some notification related stuff, and some string values.

---
+++ app/src/main/java/com/bchollywood/dailyselfieapp/NotificationSubActivity.java
@@ -0,0 +1,16 @@
+package com.bchollywood.dailyselfieapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Barry on 13/06/2015.
+ */
+public class NotificationSubActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub_activity);
+    }
+}
