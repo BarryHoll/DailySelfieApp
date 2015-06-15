b2ce0cdc00f3109328d75c787c6f3f9901ffe5a3
BarryHoll <bchollgithub@gmail.com>
2015-06-15 19:34:57 +0100

    Fixed Receiver class.

--- app/src/main/java/com/bchollywood/dailyselfieapp/DailySelfieAlarmReceiver.java
+++ app/src/main/java/com/bchollywood/dailyselfieapp/DailySelfieAlarmReceiver.java
@@ -6,29 +6,41 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 
 /**
  * Created by Barry on 14/06/2015.
  */
 public class DailySelfieAlarmReceiver extends BroadcastReceiver {
 
+    private Intent mNotificationIntent;
+    private PendingIntent mContentIntent;
+    private static final int NOTIFICATION_ID = 1;
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Intent dailySelfieActivityIntent = new Intent(context, DailySelfieActivity.class);
-        dailySelfieActivityIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        mNotificationIntent = new Intent(context, DailySelfieActivity.class);
+        mContentIntent = PendingIntent.getActivity(context, 0, mNotificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, dailySelfieActivityIntent, 0);
+        // Build the Notification
+        Uri alarm = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Notification.Builder notificationBuilder = new Notification.Builder(context)
+                .setTicker("Say cheese!")
+                .setSmallIcon(android.R.drawable.stat_sys_warning) //TODO: Change the icon...
+                .setAutoCancel(true)
+                .setContentTitle("It's selfie time!")
+                .setContentIntent(mContentIntent)
+                .setSound(alarm);
 
-        // Build notification
-        Notification notification = new Notification.Builder(context)
-                .setSmallIcon(android.R.drawable.stat_sys_warning)
-                .setContentIntent(pendingIntent)
-                .build();
+        // Get the Notification Manager
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(
+                Context.NOTIFICATION_SERVICE
+        );
 
-        // Pass the Notification to the NotificationManager:
-        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(0, notification);
+        // Pass the Notification to the Notification Manager
+        mNotificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
 
     }
 }
